85f740ff1157a37405b68e9563ddd9baab92bd0a
Snorlaxkaa <johnny941114@gmail.com>
2024-05-29 17:11:34 +0800

    b

--- src/App.js
+++ src/App.js
@@ -1,59 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
+import "./App.css";
+import MyRGBPanel from "./components/rgb-panel/MyRGBPanel"
+import MySlider from "./components/rgb-panel/MySlider";
+import MyCalculator from "./components/calculator/MyCalculator";
+import TicTacToe from "./components/tic-tac-toe/TicTacToe";
 
 function App() {
-  const [color, setColor] = useState({ r: 0, g: 0, b: 0 });
-  const [previousColor, setPreviousColor] = useState({ r: 0, g: 0, b: 0 });
-
-  const updateColor = (e) => {
-    setColor({ ...color, [e.target.name]: parseInt(e.target.value, 10) });
-  };
-
-  const saveColor = () => {
-    setPreviousColor(color);  // 將當前顏色保存為上一個顏色
-  };
-
-  const colorStyle = {
-    backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})`,
-    width: "90%",
-    height: "400px",
-    marginTop: "20px"
-  };
-
-  const previousColorStyle = {
-    color: `rgb(${previousColor.r}, ${previousColor.g}, ${previousColor.b})`, // 使用字體顏色而非背景色
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Javascript程式設計</h1>
-        <h2 style={previousColorStyle}>5B0G0017黃宗彥</h2>  
-        <div>
-          <label>R: <input type="range" name="r" min="0" max="255" value={color.r} onChange={updateColor} /></label>
-          <span>{color.r}</span>
-        </div>
-        <div>
-          <label>G: <input type="range" name="g" min="0" max="255" value={color.g} onChange={updateColor} /></label>
-          <span>{color.g}</span>
-        </div>
-        <div>
-          <label>B: <input type="range" name="b" min="0" max="255" value={color.b} onChange={updateColor} /></label>
-          <span>{color.b}</span>
-        </div>
-        <button
-  onClick={saveColor}
-  style={{
-    marginTop: '20px',
-    padding: '10px 20px',
-    fontSize: '16px',
-    borderRadius: '10px' // 調整這個值來控制圓角的大小
-  }}
->
-  保存顏色
-</button>
-        <div style={colorStyle} className="color-display">當前顏色</div>
-      </header>
+      <h1>MySlider</h1>
+      <MySlider />
+      <h1>MyRGBPanel</h1>
+      <MyRGBPanel />
+      <h1>MyCalculator</h1>
+      <MyCalculator />
+      <h1>Tic-Tac-Toe Game</h1>
+      <TicTacToe />
     </div>
   );
 }
